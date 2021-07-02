535659f6553260e337aacb61c0b07e03c8586dab
binhdz200 <86812941+binhdz200@users.noreply.github.com>
2021-07-02 13:49:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'SƠN cute ơi!',
+    introTitle: 'LONG cute ơi!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
