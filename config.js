2dad8143f144f5685ce9925709f179516f167e79
binhdz200 <86812941+binhdz200@users.noreply.github.com>
2021-07-02 09:12:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h em qua đón nhé hoàng tử.',
+    messDesc: 'Tối nay 7h Em qua đón nhé hoàng tử.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
